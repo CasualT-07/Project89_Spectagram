57934077747f29e0bb53377c468d4baa6bac8ad8
CasualT-07 <tanush89y3p@gmail.com>
2024-02-15 14:00:00 +0530

    commit

--- navigation/DrawerNavigation.js
+++ navigation/DrawerNavigation.js
@@ -1,20 +1,54 @@
-import React from 'react';
+import React, {Component} from 'react';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import Profile from '../screens/Profile';
 import Logout from '../screens/Logout';
 import StackNavigator from './StackNavigator';
+import CustomSideBarMenu from '../screens/CustomDrawer';
+
+import { getAuth } from 'firebase/auth';
+import { ref,onValue} from 'firebase/database';
+import db from '../config';
 
 const Drawer = createDrawerNavigator();
 
-const DrawerNavigator = () => {
-    return ( 
-        <Drawer.Navigator
-        initialRouteName='MyHome'>
-            <Drawer.Screen name='MyHome' component={StackNavigator} />
-            <Drawer.Screen name='Profile' component={Profile} />
-            <Drawer.Screen name='Logout' component={Logout} />
-        </Drawer.Navigator>
-    )
-}
+export default class DrawerNavigator extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            light_theme: true,
+        }
+    }
 
-export default DrawerNavigator;
+    componentDidMount() {
+        let theme;
+        const auth = getAuth();
+        const userId = auth.currentUser.uid;
+
+        onValue(ref(db, '/users/' + userId), (snapshot) => {
+            theme = snapshot.val().current_theme;
+            this.setState({
+                light_theme: theme === 'light' ? true : false,
+            })
+        })
+    }
+
+    render() {
+        let props = this.props;
+        return ( 
+            <Drawer.Navigator
+            drawerContent={(props) => <CustomSideBarMenu {...props} />}
+            screenOptions={{
+                headerShown: false,
+                drawerActiveTintColor: '#e91e63',
+                drawerInactiveTintColor: 'grey',
+                itemStyle: {marginVertical: 5},
+            }}
+            initialRouteName='MyHome'>
+                <Drawer.Screen name='MyHome' component={StackNavigator} options={{unmountOnBlur: true}}/>
+                <Drawer.Screen name='Profile' component={Profile} options={{unmountOnBlur: true}}/>
+                <Drawer.Screen name='Logout' component={Logout} options={{unmountOnBlur: true}}/>
+            </Drawer.Navigator>
+        )
+    }
+    
+}
